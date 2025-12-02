6189216e99d69d5ff96f4dee6ba9d043cabdb3ad
up1 <somkiat.p@gmail.com>
2025-12-02 23:24:53 +0700

    API versioning with query param

--- demotest/src/main/java/com/example/demotest/config/ApiConfig.java
+++ demotest/src/main/java/com/example/demotest/config/ApiConfig.java
@@ -1,10 +1,7 @@
 package com.example.demotest.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.servlet.config.annotation.ApiVersionConfigurer;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,12 +9,8 @@ public class ApiConfig implements WebMvcConfigurer {
 
     @Override
     public void configureApiVersioning(ApiVersionConfigurer configurer) {
-        configurer.usePathSegment(1);
-    }
-
-    @Override
-    public void configurePathMatch(PathMatchConfigurer configurer) {
-        configurer.addPathPrefix("/api/v{version}", HandlerTypePredicate.forAnnotation(RestController.class));
+        configurer.useQueryParam("version");
     }
 
 }
+
